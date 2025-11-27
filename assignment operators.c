cfc65d4ce0396abf1bf1d8eb3d282f3ecac7f95c
vakachandana55-cmyk <vakachandana55@gmail.com>
2025-11-27 12:39:22 +0530

    assignment operators.c

---
+++ assignment operators.c
@@ -0,0 +1,18 @@
+int main() {
+    int a = 25, b = 5;
+
+    // using operators and printing results
+    printf("a = b: %d\n", a = b);
+    printf("a += b: %d\n", a += b);
+    printf("a -= b: %d\n", a -= b);
+    printf("a *= b: %d\n", a *= b);
+    printf("a /= b: %d\n", a /= b);
+    printf("a %%= b: %d\n", a %= b);
+    printf("a &= b: %d\n", a &= b);
+    printf("a |= b: %d\n", a |= b);
+    printf("a ^= b: %d\n", a ^= b);
+    printf("a >>= b: %d\n", a >>= b); 
+    printf("a <<= b: %d\n", a <<= b);
+
+    return 0;
+}
