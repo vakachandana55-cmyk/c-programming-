771e291178af01f7f6cd785b6b344f493136058b
vakachandana55-cmyk <vakachandana55@gmail.com>
2025-11-27 16:24:05 +0530

    Add nested loops example in nested.c

---
+++ nested.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main(){
+for (int i=0;i<3;i++)
+{
+    for ( int j=0;j<2;j++)
+    {
+        printf("i=%d,j=%d \n",i,j);
+    }
+}
+return 0;
+}
