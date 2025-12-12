8422d736711bbfb4b9eb1af37fa5adac5b28ece0
vakachandana55-cmyk <vakachandana55@gmail.com>
2025-12-12 18:30:02 +0530

    Create prime num.c

---
+++ prime num.c
@@ -0,0 +1,34 @@
+// Online C compiler to run C program online
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+    int n = 91;
+
+    int cnt = 0;
+
+    
+    if (n <= 1)
+        printf("%d is NOT prime\n", n);
+    else {
+
+        
+        for (int i = 1; i <= n; i++) {
+
+
+            if (n % i == 0)
+                cnt++;
+        }
+
+
+        if (cnt > 2)
+            printf("%d is NOT prime\n", n);
+
+        
+        else
+            printf("%d is prime", n);
+    }
+
+    return 0;
+}
+    
